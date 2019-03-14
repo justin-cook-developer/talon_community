4ccd85ebfe3cf39fa3fd21f22b9a4b2316f5a053
Justin Cook <justin.cook.dev@gmail.com>
2019-03-13 18:06:31 -0700

    Removed personal JS Snippets.

--- lang/javascript.py
+++ lang/javascript.py
@@ -72,30 +72,4 @@ context.keymap({
     '(op (power | exponent) | to the power [of])': ' ** ',
     'op and': ' && ',
     'op or': ' || ',
-
-    # commands for express
-    'express import': 'ImportExpress',
-    'express initialize': 'InitializeApplication',
-    'expressive initialize': 'InitializeRouter',
-    'expressive use': 'ExpressRouterUse',
-    'express use': 'ExpressApplicationUse',
-    'express callback': 'ExpressRouteCb',
-    'expressive route': 'RouterRoute',
-    'express route': 'ApplicationRoute',
-
-    # commands for sequelize
-    'model import': 'ImportDB',
-    'model initialize': 'DBInit',
-    'model equalize': 'Sequelize',
-    'model nag': 'allowNull',
-
-    # commands for redux
-    'store import': 'ImportRedux',
-    'store logger': 'ImportLogger',
-    'store think': 'ImportThunk',
-    'store combine': 'ImportReducers',
-    'store create': 'MakeStore',
-
-    # commands for react-redux
-    'combo provider': 'ImportProvider',
 })
